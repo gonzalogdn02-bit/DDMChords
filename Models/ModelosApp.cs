93c4d99daf04ee631bb9d70840756a88b0f90195
gonza <gonza@GONZALONUEZ>
2026-06-28 01:24:01 -0300

    Agregar archivos de proyecto.

---
+++ Models/ModelosApp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DDMChords.Models
+{
+    public class Cancion
+    {
+        public string Titulo { get; set; } = "";
+        public string Artista { get; set; } = "";
+        public string TonoOriginal { get; set; } = "D";
+        public string TonoActual { get; set; } = "D";
+        public string LetraConAcordes { get; set; } = "";
+        public string NombrePdf { get; set; } = ""; // Para simular o guardar el archivo subido
+        public List<NotaBanda> Notas { get; set; } = new();
+    }
+
+    public class NotaBanda
+    {
+        public string Rol { get; set; } = "";
+        public string Texto { get; set; } = "";
+        public string Instrumento { get; internal set; }
+        public string ColorBorde { get; internal set; }
+    }
+}
---
+++ Pages/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿// Archivo: Pages/Home.razor.cs
+using System.Collections.Generic;
+using DDMChords.Models;
+
+namespace DDMChords.Pages
+{
+    public partial class Home
+    {
+        public bool UsuarioAutenticado { get; set; } = false;
+        public string instSeleccionado { get; set; } = "Guitarra Eléctrica";
+        public Cancion cancion { get; set; } = new Cancion { Titulo = "Digno", Artista = "Marcos Brunet" };
+
+        public List<string> listaInstrumentos { get; set; } = new() { "Voz Principal", "Coro 1", "Guitarra Eléctrica", "Gtr Acústica", "Teclado", "Batería" };
+        public List<string> tonosMayores { get; set; } = new() { "C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
+
+        public List<NotaBanda> listaNotas { get; set; } = new() {
+            new NotaBanda { Instrumento = "Guitarra Eléctrica", Texto = "Acompañar suave en el final junto al teclado.", ColorBorde = "#a855f7" },
+            new NotaBanda { Instrumento = "Voz Principal", Texto = "Terminamos muy suave en el cierre.", ColorBorde = "#f59e0b" }
+        };
+
+        public void Ingresar()
+        {
+            usuarioAutenticado = true;
+        }
+    }
+}
